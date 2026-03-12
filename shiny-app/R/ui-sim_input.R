c6a953c2bffd3d6fb1b978d2e7d846a19f646dcf
chengxili <chengxilii@outlook.com>
2026-03-12 17:59:11 -0400

    Update simulation inputs to sliders and change to reasonable values in ui-sim_input.R and add validation logic in site-helper.R

--- shiny-app/R/ui-sim_input.R
+++ shiny-app/R/ui-sim_input.R
@@ -1,3 +1,4 @@
+
 sim_inputs <- div(
   conditionalPanel(
     condition = "input.modify_type == 'simulation'",
@@ -7,24 +8,36 @@ sim_inputs <- div(
                  label = "Insert the seed for simulation.",
                  value = 1),
 
-    numericInput(inputId = "sample_size",
-                 label = "Insert the sample size.",
-                 value = 10000),
+    #numericInput(inputId = "sample_size",
+    #             label = "Insert the sample size.",
+    #             value = 10000),
 
-    numericInput(inputId = "num_features",
-                 label = "Insert the number of features.",
-                 value = 5,
-                 min = 2),
+    sliderInput(inputId = "sample_size",
+                label = "Select the sample size (n).",
+                min = 50, max = 500, value = 200, step = 10),
+    
+    #numericInput(inputId = "num_features",
+    #             label = "Insert the number of features.",
+    #             value = 5,
+    #             min = 2),
 
-    numericInput(inputId = "num_relevant",
-                 label = "Insert the number of true relevant features.",
-                 value = 1,
-                 min = 1),
+    sliderInput(inputId = "num_features",
+                label = "Select the number of features (p).",
+                min = 10, max = 200, value = 100, step = 5),
+    
+    #numericInput(inputId = "num_relevant",
+    #             label = "Insert the number of true relevant features.",
+    #             value = 1,
+    #             min = 1),
 
+    sliderInput(inputId = "num_relevant",
+                label = "Select the number of true relevant features (s).",
+                min = 1, max = 50, value = 5, step = 1),
+    
     numericInput(inputId = "num_permutations",
                  label = "Insert the number of permutations.",
                  value = 100,
-                 min = 1),
+                 min = 1, max = 1000),
 
     numericInput(inputId = "alpha_level",
                  label = "Insert the significance level.",
@@ -50,12 +63,12 @@ sim_graph_inputs <- div(
 
     selectInput(inputId = "selected_feature_1",
                 label = "Select feature 1 for the x-axis.",
-                choices = 1:20,  # TODO: need to be consistent with num_features input
+                choices = 1:200,  # TODO: need to be consistent with num_features input
                 selected = 1),
 
     selectInput(inputId = "selected_feature_2",
                 label = "Select feature 2 for the y-axis.",
-                choices = 1:20,  # TODO: need to be consistent with num_features input
+                choices = 1:200,  # TODO: need to be consistent with num_features input
                 selected = 2),
   ), # End conditionalPanel
 
